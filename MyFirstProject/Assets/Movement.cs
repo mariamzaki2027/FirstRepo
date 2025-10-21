85312b19ee36a8060c75615abac846f400fda6cb
mariamzaki2027 <mariam239279@bue.edu.eg>
2025-10-21 10:53:52 +0300

    pushing lab 3

---
+++ MyFirstProject/Assets/Movement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    public KeyCode Spacebar;
+    private bool grounded;
+    public KeyCode L;
+    public KeyCode R;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(Spacebar) && grounded)
+        {
+            Jump();
+        }
+
+        if (Input.GetKey(L))
+        {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+
+            if (GetComponent<SpriteRenderer>() != null)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+
+            }
+
+            if (Input.GetKey(R))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+                if (GetComponent<SpriteRenderer>() != null)
+                {
+
+                    GetComponent<SpriteRenderer>().flipX = false;
+
+                }
+            }
+
+        }
+
+
+
+
+
+        void Jump()
+        {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+}
